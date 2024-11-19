320ca5f9a1308e4e97dd486060d8fa9b51a5d1d6
olesyamaniuk <maniukolesya222@gmail.com>
2024-11-19 15:43:20 +0200

    2

--- js/slick.js
+++ js/slick.js
@@ -16,7 +16,23 @@ $(function () {
   });
 });
 
-
+$(function () {
+  $('.benefits-list').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: false,
+    centerMode: true,
+    cssEase: 'linear',
+    prevArrow: '.gallery__list-left',
+    nextArrow: '.gallery__list-right',
+    
+  });
+});
 
 
 
